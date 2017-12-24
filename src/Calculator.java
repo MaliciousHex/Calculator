20fc9f3a86f069d313023eaba732e9eee6a944aa
Harry Ermawan <harryermawan@gmail.com>
2017-12-25 03:00:34 +0700

    init

---
+++ src/Calculator.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class Calculator extends JFrame{
+	
+//	Declare
+	private JButton But1,But2,But3,But4,But5,But6,But7,But8,But9,But0,ButPlus,ButMin,ButMulti,ButDiv,ButRep,ButSqrt,ButCom,ButDel,BuatRes;
+	JTextField TFCalc;
+	
+	public void initComponents() {
+		
+	}
+	
+	public Calculator() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
